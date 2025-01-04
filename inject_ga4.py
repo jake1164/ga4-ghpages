d9921cb081f22051f741425a26951304d240282c
Jason Jackson <jake1164@hotmail.com>
2025-01-04 10:52:41 -0500

    Added initial attempt at a GA4 injection

---
+++ inject_ga4.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+tracking_id = os.getenv('INPUT_TRACKING_ID')
+file = os.getenv('INPUT_FILE')
+
+if not tracking_id:
+    print('GA4 Tracking ID not provided. Exiting script.')
+    sys.exit(1)
+
+tracking_code = f"""
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={tracking_id}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){{dataLayer.push(arguments);}}
+  gtag('js', new Date());
+  gtag('config', '{tracking_id}');
+</script>
+<!-- End Google Analytics -->
+"""
+
+# check if INPUT_FILE exists
+if not file:
+    print('File not provided. Exiting script.')
+
+if not os.path.exists(file):
+    print(f'File {file} not found. Exiting script.')
+    sys.exit(1)
+
+with open(file, 'r+') as f:
+    content = f.read()
+    if '</head>' in content:
+        content = content.replace('</head>', tracking_code + '\n</head>')
+        f.seek(0)
+        f.write(content)
+        f.truncate()
+        print(f'Tracking code injected into {file}')
+    else:
+        print(f'</head> tag not found in {file}')
+sys.exit(0)
